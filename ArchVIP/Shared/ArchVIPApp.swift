900ece2bf588b6bf732aab2575b3147d5d73b300
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2022-08-10 10:41:03 -0300

    big bang

---
+++ ArchVIP/Shared/ArchVIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchVIPApp.swift
+//  Shared
+//
+//  Created by Fernanda Andreia Nascimento on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ArchVIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
